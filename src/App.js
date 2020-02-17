ca36e2f8a29a9033c8c36e1f23a5f07ece7b3627
Adam Casey <adamcasey@Adams-MacBook-Pro.local>
2020-02-17 13:14:14 -0700

    Add state to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Products from './components/Products';
-import ProductAdmin from './components/ProductAdmin';
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
-import ChangePassword from './components/auth/ChangePassword';
-import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
-import Welcome from './components/auth/Welcome';
-import Footer from './components/Footer';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import ProductAdmin from "./components/ProductAdmin";
+import LogIn from "./components/auth/LogIn";
+import Register from "./components/auth/Register";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import ForgotPasswordVerification from "./components/auth/ForgotPasswordVerification";
+import ChangePassword from "./components/auth/ChangePassword";
+import ChangePasswordConfirm from "./components/auth/ChangePasswordConfirm";
+import Welcome from "./components/auth/Welcome";
+import Footer from "./components/Footer";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 library.add(faEdit);
 
 class App extends Component {
+  state = {
+    isAuthenticated: false,
+    user: null
+  };
+
+  // Helper functions to change state when user is logged in
+  setAuthStatus = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  };
+
+  setUser = user => {
+    this.setState({ user: user });
+  };
+
   render() {
     return (
       <div className="App">
@@ -31,9 +45,17 @@ class App extends Component {
               <Route exact path="/login" component={LogIn} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/forgotpassword" component={ForgotPassword} />
-              <Route exact path="/forgotpasswordverification" component={ForgotPasswordVerification} />
+              <Route
+                exact
+                path="/forgotpasswordverification"
+                component={ForgotPasswordVerification}
+              />
               <Route exact path="/changepassword" component={ChangePassword} />
-              <Route exact path="/changepasswordconfirmation" component={ChangePasswordConfirm} />
+              <Route
+                exact
+                path="/changepasswordconfirmation"
+                component={ChangePasswordConfirm}
+              />
               <Route exact path="/welcome" component={Welcome} />
             </Switch>
             <Footer />
--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import FormErrors from "../FormErrors";
 import Validate from "../utility/FormValidation";
+import { Auth } from "aws-amplify";
 
 class LogIn extends Component {
   state = {
@@ -34,6 +35,21 @@ class LogIn extends Component {
     }
 
     // AWS Cognito integration here
+    try {
+      const user = await Auth.signIn(this.state.username, this.state.password);
+      console.log(user);
+      // Redirect to home page
+      this.props.history.push("/");
+    } catch (error) {
+      let err = null;
+      !error.message ? (err = { message: error }) : (err = error);
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          cognito: err
+        }
+      });
+    }
   };
 
   onInputChange = event => {
@@ -53,8 +69,8 @@ class LogIn extends Component {
           <form onSubmit={this.handleSubmit}>
             <div className="field">
               <p className="control">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="text"
                   id="username"
                   aria-describedby="usernameHelp"
@@ -66,8 +82,8 @@ class LogIn extends Component {
             </div>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="password"
                   id="password"
                   placeholder="Password"
@@ -86,9 +102,7 @@ class LogIn extends Component {
             </div>
             <div className="field">
               <p className="control">
-                <button className="button is-success">
-                  Login
-                </button>
+                <button className="button is-success">Login</button>
               </p>
             </div>
           </form>

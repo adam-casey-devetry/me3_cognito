0bc04d78de36a4ac84a52cafc938769f765b96c1
Adam Casey <adamcasey@Adams-MacBook-Pro.local>
2020-02-18 09:35:29 -0700

    Make authentication status persist between pages/reloads

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ChangePassword from "./components/auth/ChangePassword";
 import ChangePasswordConfirm from "./components/auth/ChangePasswordConfirm";
 import Welcome from "./components/auth/Welcome";
 import Footer from "./components/Footer";
+import { Auth } from "aws-amplify";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 library.add(faEdit);
@@ -20,6 +21,7 @@ library.add(faEdit);
 class App extends Component {
   state = {
     isAuthenticated: false,
+    isAuthenticating: true,
     user: null
   };
 
@@ -32,6 +34,22 @@ class App extends Component {
     this.setState({ user: user });
   };
 
+  async componentDidMount() {
+    try {
+      // Retrieve session object from local storage
+      const session = await Auth.currentSession();
+      // Set isAuthenticated flag
+      this.setAuthStatus(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      // Set the user in state
+      this.setUser(user);
+    } catch (error) {
+      console.log(error);
+    }
+    this.setState({ isAuthenticating: false });
+  }
+
   render() {
     const authProps = {
       isAuthenticated: this.state.isAuthenticated,
@@ -41,71 +59,77 @@ class App extends Component {
     };
     // Use special render syntax to pass properties to each component
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar auth={authProps} />
-            <Switch>
-              {/* Pass in default props along with user created props */}
-              <Route
-                exact
-                path="/"
-                render={props => <Home {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/products"
-                render={props => <Products {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/admin"
-                render={props => <ProductAdmin {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/login"
-                render={props => <LogIn {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/register"
-                render={props => <Register {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/forgotpassword"
-                render={props => <ForgotPassword {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/forgotpasswordverification"
-                render={props => (
-                  <ForgotPasswordVerification {...props} auth={authProps} />
-                )}
-              />
-              <Route
-                exact
-                path="/changepassword"
-                render={props => <ChangePassword {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/changepasswordconfirmation"
-                render={props => (
-                  <ChangePasswordConfirm {...props} auth={authProps} />
-                )}
-              />
-              <Route
-                exact
-                path="/welcome"
-                render={props => <Welcome {...props} auth={authProps} />}
-              />
-            </Switch>
-            <Footer />
-          </div>
-        </Router>
-      </div>
+      !this.state.isAuthenticating && (
+        <div className="App">
+          <Router>
+            <div>
+              <Navbar auth={authProps} />
+              <Switch>
+                {/* Pass in default props along with user created props */}
+                <Route
+                  exact
+                  path="/"
+                  render={props => <Home {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/products"
+                  render={props => <Products {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/admin"
+                  render={props => <ProductAdmin {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  render={props => <LogIn {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  render={props => <Register {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/forgotpassword"
+                  render={props => (
+                    <ForgotPassword {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/forgotpasswordverification"
+                  render={props => (
+                    <ForgotPasswordVerification {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/changepassword"
+                  render={props => (
+                    <ChangePassword {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/changepasswordconfirmation"
+                  render={props => (
+                    <ChangePasswordConfirm {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/welcome"
+                  render={props => <Welcome {...props} auth={authProps} />}
+                />
+              </Switch>
+              <Footer />
+            </div>
+          </Router>
+        </div>
+      )
     );
   }
 }

77e46a70d3204cee814270e7b034bea0a7e11945
Adam Casey <adam.casey@devetry.com>
2020-02-19 13:42:56 -0700

    Add FB login button to Log In page

---
+++ src/components/FacebookButton.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Auth } from "aws-amplify";
+
+// Wait for the Facebook JS SDK to load
+// Once loaded, enable the Login With Facebook button
+function waitForInit() {
+  return new Promise((res, rej) => {
+    const hasFbLoaded = () => {
+      if (window.FB) {
+        res();
+      } else {
+        setTimeout(hasFbLoaded, 300);
+      }
+    };
+    hasFbLoaded();
+  });
+}
+
+export default class FacebookButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  async componentDidMount() {
+    await waitForInit();
+    this.setState({ isLoading: false });
+  }
+
+  statusChangeCallback = response => {
+    if (response.status === "connected") {
+      this.handleResponse(response.authResponse);
+    } else {
+      this.handleError(response);
+    }
+  };
+
+  checkLoginState = () => {
+    window.FB.getLoginStatus(this.statusChangeCallback);
+  };
+
+  handleClick = () => {
+    window.FB.login(this.checkLoginState, { scope: "public_profile,email" });
+  };
+
+  handleError(error) {
+    alert(error);
+  }
+
+  async handleResponse(data) {
+    const { email, accessToken: token, expiresIn } = data;
+    const expires_at = expiresIn * 1000 + new Date().getTime();
+    const user = { email };
+
+    this.setState({ isLoading: true });
+
+    try {
+      const response = await Auth.federatedSignIn(
+        "facebook",
+        { token, expires_at },
+        user
+      );
+      this.setState({ isLoading: false });
+      this.props.onLogin(response);
+    } catch (e) {
+      this.setState({ isLoading: false });
+      this.handleError(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className="fb-login-button"
+          data-width=""
+          data-size="large"
+          data-button-type="continue_with"
+          data-auto-logout-link="false"
+          data-use-continue-as="false"
+          onClick={this.handleClick}
+          disabled={this.state.isLoading}
+        ></div>
+      </div>
+    );
+  }
+}

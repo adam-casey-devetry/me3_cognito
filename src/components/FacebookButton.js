a50b671993879a4bd4e299d0c318f88f72fb62fd
Adam Casey <adam.casey@devetry.com>
2020-02-20 11:04:20 -0700

    Receive CognitoIdentityCredentials after FB login but not user data

--- src/components/FacebookButton.js
+++ src/components/FacebookButton.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { Auth } from "aws-amplify";
+// eslint-disable-next-line
+import config from "../config";
 
+/* Amplify.configure({
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: config.cogntio.IDENTITY_POOL_ID
+  }
+});
+ */
 // Wait for the Facebook JS SDK to load
 // Once loaded, enable the Login With Facebook button
 function waitForInit() {
@@ -20,7 +29,7 @@ function waitForInit() {
 export default class FacebookButton extends Component {
   constructor(props) {
     super(props);
-
+    this.signIn = this.signIn.bind(this);
     this.state = {
       isLoading: true
     };
@@ -29,37 +38,101 @@ export default class FacebookButton extends Component {
   async componentDidMount() {
     console.log("Mounted");
     await waitForInit();
+    this.createScript();
     this.setState({ isLoading: false });
   }
 
-  statusChangeCallback = response => {
-    console.log("satusChangeCallback");
-    if (response.status === "connected") {
-      this.handleResponse(response.authResponse);
-    } else {
-      this.handleError(response);
-    }
-  };
-
-  checkLoginState = () => {
-    console.log("Checking login state");
-    window.FB.getLoginStatus(this.statusChangeCallback);
-  };
-
+  // #1
   handleClick = () => {
     console.log("Login to FB attempt");
     window.FB.login(this.checkLoginState, { scope: "public_profile, email" });
   };
 
+  // #2
+  checkLoginState = () => {
+    console.log("Checking login state");
+    window.FB.getLoginStatus(this.statusChangeCallback);
+  };
+
+  // #3
+  statusChangeCallback = response => {
+    if (response.status === "connected") {
+      console.log("You are connected");
+      this.handleResponse(response.authResponse);
+    } else {
+      console.log("You're not connected!");
+      this.handleError(response);
+    }
+  };
+
+  signIn() {
+    const fb = window.FB;
+    fb.getLoginStatus(response => {
+      if (response.status === "connected") {
+        this.getAWSCredentials(response.authResponse);
+      } else {
+        fb.login(
+          response => {
+            if (!response || !response.authResponse) {
+              return;
+            }
+            this.getAWSCredentials(response.authResponse);
+          },
+          {
+            // the authorized scopes
+            scope: "public_profile,email"
+          }
+        );
+      }
+    });
+  }
+
+  getAWSCredentials(response) {
+    const { accessToken, expiresIn } = response;
+    const date = new Date();
+    const expires_at = expiresIn * 1000 + date.getTime();
+    if (!accessToken) {
+      return;
+    }
+
+    const fb = window.FB;
+    fb.api("/me", { fields: "name,email" }, response => {
+      const user = {
+        name: response.name,
+        email: response.email
+      };
+
+      Auth.federatedSignIn(
+        "facebook",
+        { token: accessToken, expires_at },
+        user
+      ).then(credentials => {
+        console.log(credentials);
+      });
+    });
+  }
+
+  createScript() {
+    // load the sdk
+    window.fbAsyncInit = this.fbAsyncInit;
+    const script = document.createElement("script");
+    script.src = "https://connect.facebook.net/en_US/sdk.js";
+    script.async = true;
+    script.onload = this.initFB;
+    document.body.appendChild(script);
+  }
+
   handleError(error) {
-    console.log(error);
+    console.log("Error encountered: " + error);
     alert(error);
   }
 
   async handleResponse(data) {
     const { email, accessToken: token, expiresIn } = data;
+    console.log("accessToken: " + token);
     const expires_at = expiresIn * 1000 + new Date().getTime();
     const user = { email };
+    console.log("User: " + user);
     this.setState({ isLoading: true });
 
     try {
@@ -72,6 +145,7 @@ export default class FacebookButton extends Component {
       this.setState({ isLoading: false });
       this.props.onLogin(response);
     } catch (e) {
+      console.log("Federated Signin attempt failed");
       this.setState({ isLoading: false });
       this.handleError(e);
     }
@@ -83,11 +157,11 @@ export default class FacebookButton extends Component {
         <div>
           <button
             type="button"
-            class="btn btn-primary"
+            className="btn btn-primary"
             data-toggle="button"
             aria-pressed="false"
-            autocomplete="off"
-            onClick={this.handleClick}
+            autoComplete="off"
+            onClick={this.signIn}
             disabled={this.state.isLoading}
           >
             Login With Facebook

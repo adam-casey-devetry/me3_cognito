c39a06831a28b30a15f4ac83f0b0ee2ef2618deb
Adam Casey <adam.casey@devetry.com>
2020-02-21 08:26:07 -0700

    Revert back to combined FB login button to test AWS Identity

--- src/components/FacebookButton.js
+++ src/components/FacebookButton.js
@@ -9,21 +9,6 @@ import config from "../config";
   }
 });
  */
-// Wait for the Facebook JS SDK to load
-// Once loaded, enable the Login With Facebook button
-function waitForInit() {
-  return new Promise((res, rej) => {
-    const hasFbLoaded = () => {
-      if (window.FB) {
-        res();
-        console.log("SDK Loaded");
-      } else {
-        setTimeout(hasFbLoaded, 100);
-      }
-    };
-    hasFbLoaded();
-  });
-}
 
 export default class FacebookButton extends Component {
   constructor(props) {
@@ -36,33 +21,9 @@ export default class FacebookButton extends Component {
 
   async componentDidMount() {
     console.log("Mounted");
-    await waitForInit();
     this.createScript();
     this.setState({ isLoading: false });
   }
-
-  // #1
-  handleClick = () => {
-    console.log("Login to FB attempt");
-    window.FB.login(this.checkLoginState, { scope: "public_profile, email" });
-  };
-
-  // #2
-  checkLoginState = () => {
-    console.log("Checking login state");
-    window.FB.getLoginStatus(this.statusChangeCallback);
-  };
-
-  // #3
-  statusChangeCallback = response => {
-    if (response.status === "connected") {
-      console.log("You are connected");
-      this.handleResponse(response.authResponse);
-    } else {
-      console.log("You're not connected!");
-      this.handleError(response);
-    }
-  };
 
   signIn() {
     const fb = window.FB;
@@ -78,7 +39,7 @@ export default class FacebookButton extends Component {
             this.getAWSCredentials(response.authResponse);
           },
           {
-            // the authorized scopes
+            // The authorized scopes
             scope: "public_profile,email"
           }
         );
@@ -87,6 +48,7 @@ export default class FacebookButton extends Component {
   }
 
   getAWSCredentials(response) {
+    console.log("Get AWS credentials");
     const { accessToken, expiresIn } = response;
     const date = new Date();
     const expires_at = expiresIn * 1000 + date.getTime();
@@ -95,77 +57,53 @@ export default class FacebookButton extends Component {
     }
 
     const fb = window.FB;
-    fb.api("/me", { fields: "name,email" }, response => {
-      const user = {
-        name: response.name,
-        email: response.email
-      };
-
-      Auth.federatedSignIn(
-        "facebook",
-        { token: accessToken, expires_at },
-        user
-      ).then(credentials => {
-        console.log("User 2: " + user.name);
-        console.log(credentials);
+    try {
+      fb.api("/me", { fields: "name,email" }, response => {
+        const user = {
+          name: response.name,
+          email: response.email
+        };
+        console.log("response.name: " + response.name);
+        console.log("Access Token: " + accessToken);
+        Auth.federatedSignIn(
+          "facebook",
+          { token: accessToken, expires_at },
+          user
+        ).then(credentials => {
+          console.log("User Name: " + user.name);
+          console.log("User Email: " + user.email);
+          console.log(credentials);
+        });
       });
-    });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   fbAsyncInit() {
-    // init the fb sdk client
+    // Init the fb sdk client
     const fb = window.FB;
     fb.init({
       appId: config.social.FB,
       cookie: true,
       xfbml: true,
-      version: "v2.11"
+      version: "v6.0"
     });
   }
 
-  initFB() {
-    const fb = window.FB;
-    console.log("FB SDK inited");
-  }
-
   createScript() {
-    // load the sdk
+    // Load the sdk
     window.fbAsyncInit = this.fbAsyncInit;
     const script = document.createElement("script");
     script.src = "https://connect.facebook.net/en_US/sdk.js";
     script.async = true;
-    script.onload = this.initFB;
-    //script.onload = this.waitForInit;
+    //script.onload = this.initFB;
     document.body.appendChild(script);
   }
 
   handleError(error) {
     console.log("Error encountered: " + error);
     alert(error);
-  }
-
-  async handleResponse(data) {
-    const { email, accessToken: token, expiresIn } = data;
-    console.log("accessToken: " + token);
-    const expires_at = expiresIn * 1000 + new Date().getTime();
-    const user = { email };
-    console.log("User: " + user);
-    this.setState({ isLoading: true });
-
-    try {
-      console.log("Calling FB");
-      const response = await Auth.federatedSignIn(
-        "Facebook",
-        { token, expires_at },
-        user
-      );
-      this.setState({ isLoading: false });
-      this.props.onLogin(response);
-    } catch (e) {
-      console.log("Federated Signin attempt failed");
-      this.setState({ isLoading: false });
-      this.handleError(e);
-    }
   }
 
   render() {
